ec2cf3724c041dff8ca9783b8bbfd2c542f31965
BacemBenDaly <bendalybacem@gmail.com>
2023-11-06 21:32:20 +0100

    Extract Script

--- src/extract.py
+++ src/extract.py
@@ -1,1 +1,16 @@
-# Empty Python file Placeholder 
+import requests
+
+def download_dataset(url: str, path: str) -> None:
+    """
+    Downloads the dataset from the provided URL and saves it to the specified path.
+    """
+    response = requests.get(url)
+    response.raise_for_status()  
+    
+    with open(path, 'wb') as f:
+        f.write(response.content)
+    print(f"Dataset downloaded and saved to {path}")
+
+if __name__ == "__main__":
+    dataset_url = 'https://ai-jobs.net/salaries/download/salaries.csv' 
+    download_dataset(dataset_url, file_path)
